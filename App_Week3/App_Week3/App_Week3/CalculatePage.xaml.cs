bc7cb6b1b6ba9f4eca8c1f61519864a99bfc4293
hadeefathima <hadeefathima10@gmail.com>
2017-12-09 09:42:54 +0530

    Task 3
    A two page application that sends parameters from one page to another

---
+++ App_Week3/App_Week3/App_Week3/CalculatePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Week3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CalculatePage : ContentPage
+	{
+		public CalculatePage (string [] str)
+		{
+			InitializeComponent ();
+            float sum = 0;float perc;int s;
+            for(int i=0;i<8;i++)
+            {
+                s = Convert.ToInt32(str[i]);
+                sum += s;
+            }
+            perc = sum / 400 * 100;
+            string p = perc.ToString();
+            percentage.Text = p;
+		}
+	}
+}
---
+++ App_Week3/App_Week3/App_Week3/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App_Week3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        public async void calculate_onClicked(object sender, EventArgs e)
+        {
+            string[] str = new string[8];
+            str[0] = sub1.Text;
+            str[1] = sub2.Text;
+            str[2] = sub3.Text;
+            str[3] = sub4.Text;
+            str[4] = sub5.Text;
+            str[5] = sub6.Text;
+            str[6] = lab1.Text;
+            str[7] = lab2.Text;
+            await Navigation.PushAsync(new CalculatePage(str));
+        }
+	}
+}
