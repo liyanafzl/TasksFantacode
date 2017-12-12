3239a2bee8fa41720a80b82d15bb158b14844a56
Liyana Fizal <liyana.fzl@gmail.com>
2017-12-12 20:35:16 +0530

    TaskViewModel

--- Week5/Week5/Week5/ViewModels/TaskViewModel.cs
+++ Week5/Week5/Week5/ViewModels/TaskViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Week5.Models;
 using Xamarin.Forms;
+using System.Windows.Input;
 
 namespace Week5.ViewModels
 {
@@ -10,26 +11,27 @@ namespace Week5.ViewModels
     {
         public TaskModel taskmodel;
         public List<TaskModel> tasks = new List<TaskModel>();
+        public ICommand addtolist { set; get; }
         public TaskViewModel()
         {
-            taskmodel = new TaskModel
-            {
-                task = taskmodel.task,
-                status = taskmodel.status,
-                priority = taskmodel.priority,
-                duedate = taskmodel.duedate,
-                notes = taskmodel.notes
-            };
-            
-             
-    }
-
-    public Command addtolist
-        {
-            set
+            //taskmodel = new TaskModel
+            //{
+            //    task = taskmodel.task,
+            //    status = taskmodel.status,
+            //    priority = taskmodel.priority,
+            //    duedate = taskmodel.duedate,
+            //    notes = taskmodel.notes
+            //};
+            addtolist = new Command(() =>
             {
                 tasks.Add(taskmodel);
-            }
-       }
+
+            });
+
+
+        }
+
+      
+    }
 }
 
