6360707590e2c5d10dfa47e3d5a45187887acd48
Liyana Fizal <liyana.fzl@gmail.com>
2017-12-22 20:23:16 +0530

    Changes in directory

---
+++ Task5/Week5/Week5/Week5/ListPageView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Week5.Models;
+using Week5.ViewModels;
+using System.Collections.ObjectModel;
+using week5.viewmodels;
+
+namespace Week5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPageView : ContentPage
+    {
+        public ListPageView(List<TaskModel> Listv)
+
+        {
+            InitializeComponent();
+            BindingContext = new ListViewModel(Listv);
+
+
+        }
+    }
+}
---
+++ Task5/Week5/Week5/Week5/ViewModels/ListViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Week5.Models;
+
+namespace week5.viewmodels
+{
+    class ListViewModel
+    {
+        private List<TaskModel> _list;
+        public List<TaskModel> Listv
+        {
+            get
+            {
+                return _list;
+            }
+            set
+            {
+                _list = value;
+            }
+        }
+        public ListViewModel(List<TaskModel> Listv)
+        {
+            this.Listv = Listv;
+
+        }
+          
+
+        //private list<taskmodel> _tasks = new list<taskmodel>();
+        //public list<taskmodel> tasks
+        //{
+        //    get
+        //    {
+        //        return _tasks;
+        //    }
+        //    set
+        //    {
+        //        _tasks = value;
+
+        //    }
+        //}
+        //public listviewmodel(list<taskmodel> list)
+        //{
+        //    this.tasks = list;
+        //}
+
+    }
+}
